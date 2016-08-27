60cc1487256af93995ec14b0863d2a0e0e0b55e9
Strider2342 <arpad.ivany@gmail.com>
2016-08-27 19:07:00 +0200

    [Add] Shader.cpp, vertex and pixel shader creation

---
+++ Engine/Shader.cpp
@@ -0,0 +1,57 @@
+#include "Shader.h"
+
+void Shader::SetInputLayout()
+{
+	D3D11_INPUT_ELEMENT_DESC ied[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 24, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+	};
+	dev->CreateInputLayout(ied, ARRAYSIZE(ied), VS->GetBufferPointer(), VS->GetBufferSize(), &pLayout);
+	devcon->IASetInputLayout(pLayout);
+}
+
+void Shader::CreateVertexShader(std::string filename, std::string entrypoint)
+{
+	ID3DBlob *errorBlob = nullptr;
+	
+	HRESULT hr = D3DCompileFromFile((LPCWSTR)filename.c_str(), nullptr, 0, entrypoint.c_str(), (dev->GetFeatureLevel() >= D3D_FEATURE_LEVEL_11_0) ? "vs_5_0" : "vs_4_0", D3DCOMPILE_DEBUG, 0, &VS, &errorBlob);
+
+	if (errorBlob)
+	{
+		OutputDebugStringA((char*)errorBlob->GetBufferPointer());
+		errorBlob->Release();
+	}
+
+	if (hr != S_OK)
+	{
+		return;
+	}
+	else
+	{
+		dev->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &pVS);
+	}
+}
+
+void Shader::CreatePixelShader(std::string filename, std::string entrypoint)
+{
+	ID3DBlob *errorBlob = nullptr;
+
+	HRESULT hr = D3DCompileFromFile((LPCWSTR)filename.c_str(), nullptr, 0, entrypoint.c_str(), (dev->GetFeatureLevel() >= D3D_FEATURE_LEVEL_11_0) ? "vs_5_0" : "vs_4_0", D3DCOMPILE_DEBUG, 0, &PS, &errorBlob);
+
+	if (errorBlob)
+	{
+		OutputDebugStringA((char*)errorBlob->GetBufferPointer());
+		errorBlob->Release();
+	}
+
+	if (hr != S_OK)
+	{
+		return;
+	}
+	else
+	{
+		dev->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &pPS);
+	}
+}
--- Engine/Shader.h
+++ Engine/Shader.h
@@ -1,10 +1,15 @@
 #pragma once
+#include <iostream>
 #include <d3d11.h>
 #include <D3D11Shader.h>
+#include <D3Dcompiler.h>
 
 class Shader
 {
 private:
+	ID3D11Device *dev;
+	ID3D11DeviceContext *devcon;
+
 	ID3D11InputLayout *pLayout = nullptr;
 
 	ID3D11VertexShader *pVS = nullptr;
@@ -12,4 +17,14 @@ private:
 	ID3D11DomainShader *pDS = nullptr;
 	ID3D11GeometryShader *pGS = nullptr;
 	ID3D11PixelShader *pPS = nullptr;
+
+	ID3DBlob *VS = nullptr;
+	ID3DBlob *PS = nullptr;
+
+public:
+	void SetInputLayout();
+	void CreateVertexShader(std::string filename, std::string entrypoint);
+	void CreatePixelShader(std::string filename, std::string entrypoint);
+
+	void Clear();
 };
