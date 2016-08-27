6cea2cdb7d91fc07b1843053087be9f683a86bb1
Strider2342 <arpad.ivany@gmail.com>
2016-08-27 22:50:35 +0200

    [Add] MeshRenderer, work in progress

---
+++ Engine/MeshRenderer.cpp
@@ -0,0 +1,43 @@
+#include "MeshRenderer.h"
+
+MeshRenderer::~MeshRenderer()
+{
+	delete material;
+
+	material = nullptr;
+}
+
+void MeshRenderer::Init(ID3D11Device *dev, ID3D11DeviceContext *devcon)
+{
+	this->dev = dev;
+	this->devcon = devcon;
+}
+
+void MeshRenderer::InitPipeline()
+{
+	D3D11_BUFFER_DESC bd;
+
+	// constant buffer		
+	ZeroMemory(&bd, sizeof(bd));
+
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(cBuffer);
+	bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+
+	dev->CreateBuffer(&bd, NULL, &pCBuffer);
+}
+
+void MeshRenderer::LoadShader()
+{
+
+}
+
+void MeshRenderer::Render()
+{
+
+}
+
+void MeshRenderer::SetMaterial(Material *material)
+{
+	this->material = material;
+}
--- Engine/MeshRenderer.h
+++ Engine/MeshRenderer.h
@@ -1,14 +1,41 @@
 #pragma once
+#include "Material.h"
+
+struct CBUFFER
+{
+	DirectX::XMMATRIX final;
+	DirectX::XMMATRIX rotation;
+	DirectX::XMVECTOR lightvector;
+	DirectX::XMVECTOR lightcolor;
+	DirectX::XMVECTOR ambientcolor;
+};
 
 class MeshRenderer
 {
 private:
+	ID3D11Device *dev;
+	ID3D11DeviceContext *devcon;
+
+	ID3D11Buffer *pVBuffer = nullptr;
+	ID3D11Buffer *pIBuffer = nullptr;
+	ID3D11Buffer *pCBuffer = nullptr;
+
+	Material *material;
+	CBUFFER cBuffer;
 
 public:
 	MeshRenderer();
+	~MeshRenderer();
 
+	void Init(ID3D11Device *dev, ID3D11DeviceContext *devcon);
 	void InitPipeline();
 	void LoadShader();
+	void CreateVertexBuffer();
+	void CreateIndexBuffer();
 
 	void Render();
+
+	Material *GetMaterial();
+
+	void SetMaterial(Material *material);
 };
