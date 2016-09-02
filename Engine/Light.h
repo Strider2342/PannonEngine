6de4a7db5deaf098af67b24cf748b7e731a6a4fc
Strider2342 <arpad.ivany@gmail.com>
2016-09-02 10:16:56 +0200

    [Add] Added Light class

---
+++ Engine/Light.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Vector3.h"
+
+class GameObject;
+class Transform;
+class Light
+{
+public:
+	static enum Type
+	{
+		Directional,
+		Point,
+		Spot
+	};
+private:
+	GameObject *gameObject = nullptr;
+	Transform *transform = nullptr;
+
+	Type type;
+
+	Vector3 color;
+	float spotAngle;
+	float attenuation;
+	float intensity;
+
+public:
+	Light();
+};
